ec2af478c2664bb0983a5979a96e069b27511b5a
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2026-04-26 19:44:13 +0200

    fix: apply ruff import sort to generated bindings
    Post-regen cleanup that alef should have done itself:
    - __init__.py: sort imports alphabetically
    - api.py: sort imports alphabetically

--- packages/python/liter_llm/api.py
+++ packages/python/liter_llm/api.py
@@ -7,7 +7,7 @@
 
 import liter_llm._internal_bindings as _rust
 
-from ._internal_bindings import DefaultClient, CustomProviderConfig
+from ._internal_bindings import CustomProviderConfig, DefaultClient
 
 
 def create_client(
